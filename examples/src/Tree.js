09833c6da9f55520059615ca29df3787a84be2f6
Муратиди Фёдор <mfs9408@yandex.ru>
2020-11-30 10:01:37 +0300

    Меняет имя первого элемента

--- examples/src/Tree.js
+++ examples/src/Tree.js
@@ -6,7 +6,7 @@ export const tree = {
     id: 100,
     avatar: avatarPersonnel,
     department: '',
-    name: 'Henry monger',
+    name: 'Henry monger Henry monger Henry monger ',
     title: 'Manager',
     totalReports: 21,
   },
